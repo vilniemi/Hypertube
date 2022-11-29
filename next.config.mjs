be775801f532a20c8f587cb4649112e6c26cbcfc
Ville Niemi <vilniemi@gmail.com>
2022-11-29 08:40:04 +0200

    cron tested and working

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
-import { env } from "./src/env/server.mjs";
+import { env } from './src/env/server.mjs';
+import { CronJob } from 'cron';
 
 /**
  * Don't be scared of the generics here.
@@ -10,20 +11,28 @@ import { env } from "./src/env/server.mjs";
  * @constraint {{import('next').NextConfig}}
  */
 function defineNextConfig(config) {
-  return config;
+	return config;
 }
-
+var job = new CronJob(
+	'* * * * * *',
+	function () {
+		console.log('You will see this message every second');
+	},
+	null,
+	true,
+	'America/Los_Angeles'
+);
 export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-  /** Next.js i18n docs:
-   * @see https://nextjs.org/docs/advanced-features/i18n-routing
-   * Reference repo for i18n:
-   * @see https://github.com/juliusmarminge/t3-i18n
-   **/
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  output: "standalone",
+	reactStrictMode: true,
+	swcMinify: true,
+	/** Next.js i18n docs:
+	 * @see https://nextjs.org/docs/advanced-features/i18n-routing
+	 * Reference repo for i18n:
+	 * @see https://github.com/juliusmarminge/t3-i18n
+	 **/
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
+	output: 'standalone',
 });
